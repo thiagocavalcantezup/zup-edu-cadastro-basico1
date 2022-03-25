e9cf5df99080031cef802054bdb65510456615b8
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-25 18:18:21 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/cadastrobasico1/CadastroBasico1Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrobasico1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroBasico1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroBasico1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/handora/cadastrobasico1/CadastroBasico1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrobasico1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroBasico1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
