2a832960ca606bddd50239cb4bb916ef4483b242
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-31 10:59:02 -0300

    Remove unnecessary @Repository annotation

--- src/main/java/br/com/zup/handora/cadastrobasico1/repositories/CarroRepository.java
+++ src/main/java/br/com/zup/handora/cadastrobasico1/repositories/CarroRepository.java
@@ -1,11 +1,9 @@
 package br.com.zup.handora.cadastrobasico1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.zup.handora.cadastrobasico1.models.Carro;
 
-@Repository
 public interface CarroRepository extends JpaRepository<Carro, Long> {
 
 }
