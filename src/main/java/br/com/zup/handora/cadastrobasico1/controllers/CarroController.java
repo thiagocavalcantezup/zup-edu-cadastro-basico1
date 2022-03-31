5c4b3667cdfc54329f8d130a54c8c711ca3b2938
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-31 10:25:41 -0300

    Add repository dependencies to Carro Controller

--- src/main/java/br/com/zup/handora/cadastrobasico1/controllers/CarroController.java
+++ src/main/java/br/com/zup/handora/cadastrobasico1/controllers/CarroController.java
@@ -3,10 +3,21 @@ package br.com.zup.handora.cadastrobasico1.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.handora.cadastrobasico1.repositories.CarroRepository;
+import br.com.zup.handora.cadastrobasico1.repositories.ClienteRepository;
+
 @RestController
 @RequestMapping(ClienteController.BASE_URI + "/{clienteId}" + CarroController.BASE_URI)
 public class CarroController {
 
     public final static String BASE_URI = "/carros";
+
+    private final ClienteRepository clienteRepository;
+    private final CarroRepository carroRepository;
+
+    public CarroController(ClienteRepository clienteRepository, CarroRepository carroRepository) {
+        this.clienteRepository = clienteRepository;
+        this.carroRepository = carroRepository;
+    }
 
 }
