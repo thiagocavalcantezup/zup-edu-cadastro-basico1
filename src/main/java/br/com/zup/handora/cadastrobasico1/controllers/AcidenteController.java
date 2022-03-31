d6428e0726269c1a7b1e06620e152908532c7532
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-31 11:22:24 -0300

    Add repository dependencies to Acidente controller

--- src/main/java/br/com/zup/handora/cadastrobasico1/controllers/AcidenteController.java
+++ src/main/java/br/com/zup/handora/cadastrobasico1/controllers/AcidenteController.java
@@ -3,10 +3,22 @@ package br.com.zup.handora.cadastrobasico1.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.handora.cadastrobasico1.repositories.AcidenteRepository;
+import br.com.zup.handora.cadastrobasico1.repositories.CarroRepository;
+
 @RestController
 @RequestMapping(CarroController.BASE_URI + "/{carroId}" + AcidenteController.BASE_URI)
 public class AcidenteController {
 
     public final static String BASE_URI = "/acidentes";
+
+    private final CarroRepository carroRepository;
+    private final AcidenteRepository acidenteRepository;
+
+    public AcidenteController(CarroRepository carroRepository,
+                              AcidenteRepository acidenteRepository) {
+        this.carroRepository = carroRepository;
+        this.acidenteRepository = acidenteRepository;
+    }
 
 }
